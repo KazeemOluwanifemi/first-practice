85b20671dfb2238429253b73de69fa347512d5bd
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 17:34:55 +0100

    conditional statements

--- first.java
+++ first.java
@@ -5,16 +5,12 @@ public class first{
         Scanner input = new Scanner(System.in);
 //        Calculating the user-specified number of days to seconds
 
-        System.out.println("Give the first number: ");
-        int firstNumber = Integer.valueOf(input.nextLine());
-        System.out.println(firstNumber);
+        System.out.println("Give speed value: ");
+        int speed = input.nextInt();
 
-        System.out.println("Give the second number: ");
-        int secondNumber = Integer.valueOf(input.nextLine());
-        System.out.println(secondNumber);
-
-        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is " + (firstNumber + secondNumber));
-//        System.out.println(firstNumber "+" secondNumber);
+        if(speed == 120){
+            System.out.println("Speeding Ticket!");
+        }
 
 
 
