e0a105b88ab39fab89c22b3d519a30ed7ce9ec98
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 17:47:45 +0100

    grade and grade point with conditional statements

--- first.java
+++ first.java
@@ -5,14 +5,25 @@ public class first{
         Scanner input = new Scanner(System.in);
 //        Calculating the user-specified number of days to seconds
 
-        System.out.println("Give speed value: ");
-        int speed = input.nextInt();
+        System.out.println("What is your grade?");
+        int grade = input.nextInt();
 
-        if(speed == 120){
-            System.out.println("Speeding Ticket!");
+        if(grade < 0){
+            System.out.println("Impossible!");
+        } else if(grade <= 49) {
+            System.out.println("failed");
+        } else if(grade <= 59) {
+            System.out.println("Your point is 1");
+        } else if(grade <= 69) {
+            System.out.println("Your point is 2");
+        } else if(grade <= 79) {
+            System.out.println("Your point is 3");
+        } else if(grade <= 89) {
+            System.out.println("Your point is 4");
+        } else if(grade <= 100) {
+            System.out.println("Your point is 5");
+        } else {
+            System.out.println("Incredible");
         }
-
-
-
     }
 }
