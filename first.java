cce411e0f59319b1e1f9e31e848102579445a4bb
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 12:01:10 +0100

    Reading double data type from the user

--- first.java
+++ first.java
@@ -4,22 +4,10 @@ public class first{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 //        Reading different variable types from users
-//        String input
-        System.out.println("Please enter your name: ");
-        String textMessage = input.nextLine();
-        System.out.println(textMessage);
-
-//        String valueAsString = "This";
-//        int value = Integer.valueOf(valueAsString); -> doesn't work cause Integer.valueOf can only convert
-//                                                       strings that represents valid numbers
-//        System.out.println(value);
-
-
-//        Integer input
-        System.out.println("Please enter your age: ");
-//        Data type conversion & reading user input
-        Integer age = Integer.valueOf(input.nextLine());
-        System.out.println(age);
+//        Integer Input
+        System.out.println("Enter an integer value: ");
+        double value = Double.valueOf(input.nextLine());
+        System.out.println("You gave the value " + value);
 
 
     }
