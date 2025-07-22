7735a4bfe5251a9a4c372b104027ac4a5bf4e919
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 21:11:42 +0100

    gift tax calculator

--- first.java
+++ first.java
@@ -4,15 +4,23 @@ public class first{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 //        Calculating the user-specified number of days to seconds
-        System.out.println("Give a year: ");
-        int year = input.nextInt();
+        System.out.println("What is the value of the gift? ");
+        int gift = input.nextInt();
 
-        if(year % 100 == 0 && year % 400 == 0){
-            System.out.println("It is a leap year");
-        }else if(year % 4 == 0) {
-            System.out.println("It is a leap year");
-        } else{
-            System.out.println("It is not a leap year");
+        if (gift < 5000){
+            System.out.println("no tax");
+        } else if(gift <= 25000){
+            System.out.println("Tax: " + (100 + (gift - 5000) * 0.08));
+        } else if(gift <= 55000){
+            System.out.println("Tax: " + (1700 + (gift - 25000) * 0.1));
+        } else if(gift <= 200000){
+            System.out.println("Tax: " + (4700 + (gift - 55000) * 0.12));
+        } else if(gift <= 1000000){
+            System.out.println("Tax: " + (22100 + (gift - 200000) * 0.15));
+        } else if(gift > 1000000){
+            System.out.println("Tax: " + (142100 + (gift - 1000000) * 0.17));
+        } else {
+            System.out.println("Invalid input");
         }
     }
 }
