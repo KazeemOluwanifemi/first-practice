c3dbf777c4b4e83774576e8c6266815e245ba07a
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 12:16:45 +0100

    different types of input

--- first.java
+++ first.java
@@ -5,13 +5,22 @@ public class first{
         Scanner input = new Scanner(System.in);
 //        Reading different variable types from users
 //        Boolean Input
-        System.out.println("Write a true/false question: ");
-        String userQuestion = input.nextLine();
-        System.out.println("What is your answer, true/false?");
+        System.out.println("Give an integer: ");
+        int userInteger = Integer.valueOf(input.nextLine());
 
-        boolean userAnswer = Boolean.valueOf(input.nextLine());
+        System.out.println("Give a string: ");
+        String userString = input.nextLine();
 
-        System.out.println("Your answer is: " + userAnswer);
+        System.out.println("Give a decimal value: ");
+        double userDouble = Double.valueOf(input.nextLine());
+
+        System.out.println("Give a boolean value: ");
+        boolean userBoolean = Boolean.valueOf(input.nextLine());
+
+        System.out.println("Your integer value is " + userInteger);
+        System.out.println("Your string value is " + userString);
+        System.out.println("Your decimal value is " + userDouble);
+        System.out.println("Your boolean value is " + userBoolean);
 
     }
 }
