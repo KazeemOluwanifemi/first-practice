40fce01715316ff06200bc92d7d2346815273efc
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-22 17:57:40 +0100

    Odd or Even number checker

--- first.java
+++ first.java
@@ -4,26 +4,15 @@ public class first{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 //        Calculating the user-specified number of days to seconds
+        System.out.println("Enter a number: ");
+        int value = input.nextInt();
 
-        System.out.println("What is your grade [0-100]?");
-        int grade = input.nextInt();
-
-        if(grade < 0){
-            System.out.println("Impossible!");
-        } else if(grade <= 49) {
-            System.out.println("failed");
-        } else if(grade <= 59) {
-            System.out.println("Your point is 1");
-        } else if(grade <= 69) {
-            System.out.println("Your point is 2");
-        } else if(grade <= 79) {
-            System.out.println("Your point is 3");
-        } else if(grade <= 89) {
-            System.out.println("Your point is 4");
-        } else if(grade <= 100) {
-            System.out.println("Your point is 5");
-        } else {
-            System.out.println("Incredible");
+        if(value % 2 == 0){
+            System.out.println(value + " is an even number");
+        } else if(value % 2 == 1){
+            System.out.println(value + " is an odd number");
+        } else{
+            System.out.println("Enter a valid number");
         }
     }
 }
