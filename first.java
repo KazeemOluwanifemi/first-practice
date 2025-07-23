e00a76c6c4428682898ab37b5ed213dc1ebe756a
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-23 11:27:23 +0100

    program to find the square and square root of numbers

--- first.java
+++ first.java
@@ -2,25 +2,20 @@ import java.util.Scanner;
 
 public class first{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-//        Calculating the user-specified number of days to seconds
-        System.out.println("What is the value of the gift? ");
-        int gift = input.nextInt();
+//        user input variable
+        Scanner reader = new Scanner(System.in);
 
-        if (gift < 5000){
-            System.out.println("no tax");
-        } else if(gift <= 25000){
-            System.out.println("Tax: " + (100 + (gift - 5000) * 0.08));
-        } else if(gift <= 55000){
-            System.out.println("Tax: " + (1700 + (gift - 25000) * 0.1));
-        } else if(gift <= 200000){
-            System.out.println("Tax: " + (4700 + (gift - 55000) * 0.12));
-        } else if(gift <= 1000000){
-            System.out.println("Tax: " + (22100 + (gift - 200000) * 0.15));
-        } else if(gift > 1000000){
-            System.out.println("Tax: " + (142100 + (gift - 1000000) * 0.17));
-        } else {
-            System.out.println("Invalid input");
-        }
+//        A program to calculate the square of a user specified integer
+        System.out.println("What number would you like to find the square of?");
+        int input1 = reader.nextInt();
+        System.out.println("The square of " + input1 + " is " + input1 * input1);
+
+        System.out.println("Enter two random numbers: ");
+        int input2 = reader.nextInt();
+        int input3 = reader.nextInt();
+
+        int sum = input2 + input3;
+        double sqrt = Math.sqrt(sum);
+        System.out.println("The square root of the sum of " + input2 + " and " + input3 + " is " + sqrt);
     }
 }
