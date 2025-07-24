5f0f00c216f832ce132012c80a42bce41949dce5
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-24 08:43:36 +0100

    Factorial of a number inputted by the user

--- first.java
+++ first.java
@@ -5,13 +5,14 @@ public class first{
 //       The reader variable
         Scanner reader = new Scanner(System.in);
 //        user input
-        System.out.println("Last number of the sequence?");
-        int last = reader.nextInt();
-        int sum = 0;
-
-        for(int i = 1; i <= last; i++){
-            sum = sum + i;
+        System.out.println("Give a number: ");
+        int number = reader.nextInt();
+        int product = 1;
+//        Loop implementation
+        for(int i = 1; i <=number; i++){
+            product *= i;
         }
-        System.out.println("The sum is " + sum);
+        System.out.println("The factorial of " + number + " is " + product);
+
     }
 }
