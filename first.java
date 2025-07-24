e123b290032b2bc6ff67bafc0f87c70ddc55033d
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-24 09:22:28 +0100

    Repeating, breaking and remembering

--- first.java
+++ first.java
@@ -4,15 +4,38 @@ public class first{
     public static void main(String[] args){
 //       The reader variable
         Scanner reader = new Scanner(System.in);
-//        user input
-        System.out.println("Give a number: ");
-        int number = reader.nextInt();
-        int product = 1;
-//        Loop implementation
-        for(int i = 1; i <=number; i++){
-            product *= i;
+//
+        int sum = 0;
+        int counter = 0;
+        int even = 0;
+        int odd = 0;
+
+        while(true){
+            System.out.println("Give a number[enter -1 to stop]: ");
+            int number = reader.nextInt();
+            sum += number;
+            counter++;
+
+            if(number == -1){
+                System.out.println("Thx, bye!");
+                break;
+            }
+
+            if (number % 2 == 0){
+                even++;
+                continue;
+            }
+
+            if(number % 2 != 0){
+                odd++;
+            }
         }
-        System.out.println("The factorial of " + number + " is " + product);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + counter);
+        System.out.println("Average: " + (1.0 * sum/counter));
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
 
     }
 }
