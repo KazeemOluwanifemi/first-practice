f9cb477bb042a82b16e38aae7f1eb1d3f580a15f
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-24 09:39:06 +0100

    Methods

--- first.java
+++ first.java
@@ -4,38 +4,18 @@ public class first{
     public static void main(String[] args){
 //       The reader variable
         Scanner reader = new Scanner(System.in);
-//
-        int sum = 0;
-        int counter = 0;
-        int even = 0;
-        int odd = 0;
-
-        while(true){
-            System.out.println("Give a number[enter -1 to stop]: ");
-            int number = reader.nextInt();
-            sum += number;
-            counter++;
-
-            if(number == -1){
-                System.out.println("Thx, bye!");
-                break;
-            }
-
-            if (number % 2 == 0){
-                even++;
-                continue;
-            }
-
-            if(number % 2 != 0){
-                odd++;
-            }
+//        Ask user for number of iterations
+        System.out.println("How many times?");
+        int input = reader.nextInt();
+        int i = 1;
+        while(i <= input){
+            printText();
+            i++;
         }
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + counter);
-        System.out.println("Average: " + (1.0 * sum/counter));
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+    }
 
+    public static void printText(){
+        System.out.println("In a hole in the ground, there lived a method.");
     }
 }
