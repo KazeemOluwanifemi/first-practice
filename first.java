65b8eb40e0f6c16504154817d87c12054d3a4759
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-25 23:04:47 +0100

    implementing lists

--- first.java
+++ first.java
@@ -1,34 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class first{
-
-
 //    main method in the call stack
     public static void main(String[] args){
-//        Specify the number of the stars
-        printStar(4);
-    }
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> wordList = new ArrayList<>();
+        int i = 0;
+        while(true){
+            System.out.println("Enter the names of employees: ");
+            String name = reader.nextLine();
 
-//    Method to print star
-    public static void printStar(int height){
-        int count = height;
-        for(int i = 1; i <= count; i++){
-            printSpaces(height);
-            height--;
-            printStars(i);
+            if(name.isEmpty()){
+                System.out.println("invalid entry");
+                break;
+            } else {
+                 wordList.add(name);
+                 i++;
+            }
         }
-    }
 
-//    Helper method to print a single line of stars
-    public static void printStars(int n){
-        for(int i = 0; i < n; i++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-
-//    Method to print spaces
-    public static void printSpaces(int n){
-        for (int i = n; i > 0; i--){
-            System.out.print(" ");
-        }
+        System.out.println(wordList.get(2));
     }
 }
