6ce206101d5359b9c5ab5cb58b961ce35facfcaa
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-27 23:01:43 +0100

    printing last element in a user determined list

--- first.java
+++ first.java
@@ -5,21 +5,32 @@ import java.util.ArrayList;
 public class first{
 //    main method in the call stack
     public static void main(String[] args){
+//        System.out.println("Name of last user is: " + lastElementOfList());
+        lastElementOfList();
+    }
+
+    public static void lastElementOfList(){
         Scanner reader = new Scanner(System.in);
+        ArrayList<String> users = new ArrayList<>();
+        System.out.println("Enter each name of the users[enter an empty value to stop loop]: ");
+        String name = reader.nextLine();
+        users.add(name);
 
-//        A program to iterate over a list using a for loop
-        ArrayList<Integer> list = new ArrayList<>();
-
-        list.add(26);
-        list.add(74);
-        list.add(32);
-        list.add(49);
-        list.add(14);
-
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
+        for(int i = 0; i < users.size(); i++){
+            name = reader.nextLine();
+            if(name.isEmpty()){
+                System.out.println("Loop ends now");
+                break;
+            } else{
+                users.add(name);
+            }
         }
-
+        int lastUserIndex = users.size() - 1;
+        System.out.println(users.get(lastUserIndex));
+//        int indexOfLast = users.size() - 1;
+//
+//        String lastName = users.get(indexOfLast);
+//        System.out.println(lastName);
     }
 
 
