9aeb15a90d3c663cdfafc80455385db612fdc8c9
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-27 23:46:35 +0100

    printing a user defined range of numbers from a list

--- first.java
+++ first.java
@@ -11,23 +11,47 @@ public class first{
 
     public static void lastElementOfList(){
         Scanner reader = new Scanner(System.in);
-        ArrayList<String> users = new ArrayList<>();
-        System.out.println("Enter each name of the users[enter an empty value to stop loop]: ");
-        String name = reader.nextLine();
-        users.add(name);
+        ArrayList<Integer> users = new ArrayList<>();
+        System.out.println("Enter random numbers[enter -1 to stop loop]: ");
+        int number = reader.nextInt();
+        users.add(number);
 
         for(int i = 0; i < users.size(); i++){
-            name = reader.nextLine();
-            if(name.isEmpty()){
+            number = reader.nextInt();
+            if(number == -1){
                 System.out.println("Loop ends now");
+
                 break;
             } else{
-                users.add(name);
+                users.add(number);
             }
+
         }
-        int lastUserIndex = users.size() - 1;
-        System.out.println("The first name on the list is " + users.get(0));
-        System.out.println("The last name on the list is " + users.get(lastUserIndex));
+        System.out.println("From where?");
+        int lower = reader.nextInt();
+        System.out.println("To where?");
+        int higher = reader.nextInt();
+
+        for(int i = lower; i <= higher; i++){
+            System.out.println(users.get(i));
+        }
+//        for(int i = 0; i <users.size(); i++){
+//            System.out.println(users.get(i));
+//        }
+
+//        System.out.println(users.get(0,5));
+//        int i = 0;
+//        while(!users.isEmpty()){
+//            if(i < users.size()){
+//                i++;
+//            } else{
+//                break;
+//            }
+//            System.out.println(users.get(i));
+//        }
+//        int lastUserIndex = users.size() - 1;
+//        System.out.println("The numbers are: " + users);
+//        System.out.println("The last name on the list is " + users.get(lastUserIndex));
     }
 
 
