7df745ba1f901ebac2eac18ae7821490ec27d415
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-28 09:34:02 +0100

    remove the last parameter of a list by passing the list as a parameter to a custom method

--- first.java
+++ first.java
@@ -5,32 +5,20 @@ import java.util.ArrayList;
 public class first{
 //    main method in the call stack
     public static void main(String[] args){
-//        System.out.println("Name of last user is: " + lastElementOfList());
-        methodOfLists();
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Bola");
+        list.add("Bolu");
+        list.add("Teni");
+        list.add("Helen");
+        removeLast(list);
+
+        System.out.println(list);
     }
 
-    public static void methodOfLists(){
-        ArrayList<String> list = new ArrayList<>();
-        Scanner reader = new Scanner(System.in);
-
-//        User fills up the list
-        System.out.println("Who are the members of the club? [Enter an empty space to end loop]");
-        String input;
-        while (true) {
-            input = reader.nextLine();
-
-            if (input.isEmpty()) {
-                break; // Exit when empty line is entered
-            }
-
-            list.add(input); // Add member to list
-        }
-//        check if a value exists in the list
-        System.out.println("Who are you looking for?");
-        input = reader.nextLine();
-        if(list.contains(input)){
-            System.out.println(input + " is on the list");
-        }
+    public static void removeLast(ArrayList<String>list){
+        int indexOfLast = list.size() - 1;
+        list.remove(indexOfLast);
     }
 
 
