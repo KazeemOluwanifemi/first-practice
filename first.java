6fc658f6929e26933fcc61b60b55898a0d3a92a1
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-28 09:56:41 +0100

    swapping values of an array

--- first.java
+++ first.java
@@ -1,29 +1,33 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class first{
 //    main method in the call stack
-    public static void main(String[] args){
-        ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 3;
+        numbers[2] = 5;
+        numbers[3] = 7;
+        numbers[4] = 9;
 
-        list.add(34);
-        list.add(50);
-        list.add(32);
-        list.add(23);
-        list.add(15);
-
-        System.out.println("The sum of numbers in this list is: " + sum(list));
-
-//        System.out.println(list);
-    }
-
-    public static Integer sum(ArrayList<Integer>list){
-        int sum = 0;
-        for(int i = 0; i < list.size(); i++){
-            sum = sum + list.get(i);
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
         }
-        return sum;
+
+        System.out.println("Give two indices to swap: ");
+        int firstNumber = reader.nextInt();
+        int secondNumber = reader.nextInt();
+
+        int helper = numbers[firstNumber];
+        numbers[firstNumber] = numbers[secondNumber];
+        numbers[secondNumber] = helper;
+
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
     }
-
-
 }
