8da4d2a1d327661716002e1994f96137eba86113
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-28 10:41:28 +0100

    splitting a string

--- first.java
+++ first.java
@@ -7,25 +7,17 @@ public class first{
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-
-        String correctUser = "alex";
-        String correctUser2 = "emma";
-        String correctPassword = "sunshine";
-        String correctPassword2 = "haskell";
-
         while(true){
-            System.out.print("Enter username: ");
-            String userName = reader.nextLine();
-            System.out.print("Enter password: ");
-            String passWord = reader.nextLine();
-            if(userName.equals(correctUser) && passWord.equals(correctPassword)){
-                System.out.print("You have logged in successfully!");
-                break;
-            } else if(userName.equals(correctUser2) && passWord.equals(correctPassword2)){
-                System.out.print("You have logged in successfully!");
+            System.out.println("Enter a sentence[give an empy input to end the loop]: ");
+            String user = reader.nextLine();
+
+            if(user.isEmpty()){
                 break;
             } else{
-                System.out.println("Incorrect username or password");
+                String[] pieces = user.split(" ");
+                for(int i = 0; i < pieces.length; i++){
+                    System.out.println(pieces[i]);
+                }
             }
         }
     }
