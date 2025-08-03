37e049d4696c296f64ea8c35a5211537a78274b1
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-08-03 23:43:53 +0100

    updates error in array handling in class file

--- FileHandling.java
+++ FileHandling.java
@@ -1,4 +1,95 @@
-package PACKAGE_NAME;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class FileHandling {
+    class gameData{
+        Scanner reader = new Scanner(System.in);
+
+        String homeTeam;
+        String visitingTeam;
+        int homePoint;
+        int visitingPoint;
+
+//        method for getting home team names
+        ArrayList<String> getHomeTeam(String filename){
+            ArrayList<String> homeTeams = new ArrayList<>();
+
+            try (Scanner reader = new Scanner(Paths.get(filename))) {
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+
+                    String[] parts = line.split(",");
+                    homeTeam = parts[0];
+
+                    homeTeams.add(homeTeam);;
+                }
+            }
+            catch(IOException e){
+                System.out.println("Error while handling file: " + filename);
+            }
+
+            return homeTeams;
+        }
+
+        ArrayList<String> getVisitingTeam(String filename){
+            ArrayList<String> visitingTeams = new ArrayList<>();
+
+            try(Scanner reader = new Scanner(Paths.get(filename))){
+                while (reader.hasNextLine()){
+                    String line = reader.nextLine();
+
+                    String[] parts = line.split(",");
+                    visitingTeam = parts[1];
+
+                    visitingTeams.add(visitingTeam);
+                }
+            }
+            catch(IOException e){
+                System.out.println("Error while handling file: " + filename);
+            }
+            return visitingTeams;
+        }
+
+        ArrayList<Integer> getHomePoints(String filename){
+            ArrayList<Integer> homePoints = new ArrayList<>();
+
+            try(Scanner reader = new Scanner(Paths.get(filename))){
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+
+                    String[] parts = line.split(",");
+
+                    homePoint = Integer.parseInt(parts[2]);
+
+                    homePoints.add(homePoint);
+                }
+            }
+            catch(IOException e){
+                System.out.println("Error while handling file: " + filename);
+            }
+            return homePoints;
+        }
+
+        ArrayList<Integer> getVisitingPoints(String filename){
+            ArrayList<Integer> visitingPoints = new ArrayList<>();
+
+            try(Scanner reader = new Scanner(Paths.get(filename))){
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+
+                    String[] parts = line.split(",");
+
+                    visitingPoint = Integer.parseInt(parts[3]);
+
+                    visitingPoints.add(visitingPoint);
+                }
+            }
+            catch(IOException e){
+                System.out.println("Error while handling file: " + filename);
+            }
+            return visitingPoints;
+        }
+    }
 }
--- first.java
+++ first.java
@@ -1,23 +1,38 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class first{
 //    main method in the call stack
     public static void main(String[] args) {
+
+//        Instantiate class object
+        FileHandling dataOfGame = new FileHandling();
+        FileHandling.gameData data = dataOfGame.new gameData();
+//        create a list elements to store each list set
+
+        ArrayList<String> homeTeams = data.getHomeTeam("questions.csv");
+        ArrayList<String> visitingTeams = data.getVisitingTeam("questions.csv");
+        ArrayList<Integer> homePoints = data.getHomePoints("questions.csv");
+        ArrayList<Integer> visitingPoints = data.getVisitingPoints("questions.csv");
+
         Scanner reader = new Scanner(System.in);
 
-        while(true){
-            System.out.println("Enter a sentence[give an empty input to end the loop]: ");
-            String user = reader.nextLine();
+        System.out.println("Enter the name of a team: ");
+        String teamName = reader.nextLine();
+        int noOfGamesPlayed = 0;
+        int noOfGamesWon = 0;
+//        int noOfGamesLost = 0;
 
-            if(user.isEmpty()){
-                break;
-            } else{
-                String[] pieces = user.split(" ");
-                int last = pieces.length -1;
-                System.out.println(pieces[last]);
+        for(int i = 0; i < homeTeams.size(); i++){
+            if(homeTeams.get(i).equals(teamName)){
+                if(homePoints.get(i) > visitingPoints.get(i)){
+                    noOfGamesWon = noOfGamesWon + 1;
+                }
+                noOfGamesPlayed = noOfGamesPlayed + 1;
             }
         }
+
+        System.out.println("The number of games played by " + teamName + " is " + noOfGamesPlayed);
+        System.out.println("The number of games won by " + teamName + " is " + noOfGamesWon);
     }
 }
